70ab7237c6fe4f03484ffc785f4bb41578580e26
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-06-27 12:01:09 -0400

    pray for jsx

--- dr_jordan_bank/src/components/Appointments.vue
+++ dr_jordan_bank/src/components/Appointments.vue
@@ -1,9 +1,45 @@
-<script setup>
+<script>
+import {onMounted, ref} from 'vue'
+import axios from 'axios'
+
+
+export default {
+
+  setup() {
+   const appointments=ref([])
+   onMounted(async () => {
+     await axios
+         .get('https://drjordanbankinfo.herokuapp.com/api/appointments')
+         .then(response => {
+           appointments.value = response.data
+
+         })
+   })
+    return {
+     appointments
+    }
+  }
+}
+
+
 </script>
 
 <template>
-    <p>Show Appointments Here</p>
+<h1>Appointments</h1>
+<h3>Upcoming</h3>
+{{appointments.map((appointment) => {
+    return (
+        <>
+        shit
+        </>
+    )
+})}}
+<h3>Previous</h3>
+{{appointments}}
 </template>
 
 <style scoped>
+* {
+    text-align: center;
+}
 </style>
