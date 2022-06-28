15eb2c229568a1f858c613578b6ebd625330bdb1
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-06-28 11:35:06 -0400

    post is working!

--- dr_jordan_bank/src/components/Appointments.vue
+++ dr_jordan_bank/src/components/Appointments.vue
@@ -1,26 +1,43 @@
 <script>
 import {onMounted, ref} from 'vue'
-import axios from 'axios'
 
-
+import axios from "axios"
 export default {
-
-  setup() {
-   const appointments=ref([])
-   onMounted(async () => {
-     await axios
-         .get('https://drjordanbankinfo.herokuapp.com/api/appointments')
-         .then(response => {
-           appointments.value = response.data
-
-         })
-   })
+  name: "Appointments",
+  data() {
     return {
-     appointments
+      appointments: []
     }
-  }
+  },
+  methods: {
+async getAppointments() {
+      try {
+        const appointments = await axios.get(
+          "https://drjordanbankinfo.herokuapp.com/api/appointments"
+        )
+        this.appointments = appointments.data
+    } catch (err) {
+        console.log(err)
+      }
+    },
+    async makeAppointment() {
+      try {
+        const app = await axios.post(
+          "https://drjordanbankinfo.herokuapp.com/api/appointments",
+          {
+            physician: this.physician,
+            name : this.name,
+            age: this.age,
+            reason: this.reason,
+            details: this.details
+          }
+        )
+    } catch(error) {
+        console.log(error);
+      }
+    },
+  },
 }
-
 
 </script>
 
@@ -34,6 +51,31 @@ export default {
 </div>
 </div>
 <h3>Previous</h3>
+<button @click='getAppointments'>Get</button>
+<form v-on:submit='makeAppointment'>
+    <label for='physicians'>Doctor: </label>
+    <select id='physicians' v-model='physician'>
+        <option>Jordan Bank</option>
+        <option>Meiling Li</option>
+        <option>Tyger Salters</option>
+        <option>Ofori Manson</option>
+        <option>Kevin Ha</option>
+        <option>Jean Eng</option>
+        <option>Rafay Hassan</option>
+    </select><br/>
+    Your Name: <input type = 'text' v-model='name'><br/>
+    Your Age: <input type = 'number' v-model='age'><br/>
+    <label for='reason'>Reason for Visit: </label>
+    <select id='reason' v-model='reason'>
+        <option>Plastic Surgery</option>
+        <option>Necromancy</option>
+        <option>Allergies</option>
+        <option>Jerusalem Syndrome</option>
+        <option>Ate Too Much</option>
+    </select><br/>
+    Details: <input type='text' v-model='details'><br/>
+    <input type='submit' value='Make Appointment' @click='makeAppointment'>
+</form>
 </template>
 
 <style scoped>
