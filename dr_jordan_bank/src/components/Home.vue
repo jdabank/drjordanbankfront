a957b0f482b235bd99bc2ae7f3f6fea5b837bc1a
Jordan Bank <jordanbank@Jordans-MacBook-Air.local>
2022-07-05 17:03:25 -0400

    working

--- dr_jordan_bank/src/components/Home.vue
+++ dr_jordan_bank/src/components/Home.vue
@@ -51,7 +51,7 @@ methods: {
 </script>
 <script setup>
 import {onMounted, ref, reactive} from 'vue'
-const login = ref(true)
+const loginner = ref(true)
 const registration = ref(false)
 
 const registrationToggle = () => {
@@ -61,14 +61,14 @@ const registrationToggle = () => {
 
 const loginToggle = () => {
     registration.value = false
-    login.value = true
+    loginner.value = true
 }
 </script>
 
 <template>
     <h1>Welcome to Dr. Jordan Bank & Associates</h1>
     <h3>For all of your medical needs, so long as your medical needs are one of these five things.</h3>
-    <div v-if='login'>
+    <div v-if='loginner'>
       <form @submit.prevent="login">
         <h2>Login</h2>
         <input
